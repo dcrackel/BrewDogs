dcabf1b19d6b69156d0b369ffbe41078d404c37e
Dwain Crackel <dcrackel@gmail.com>
2021-11-30 15:54:55 -0500

    small changes forgot to check in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import logo from '../img/brewdoglogo.jpg';
+import {Container} from "@material-ui/core";
 
 const Header = ({ title }) => {
     return (
         <header >
+            <Container className="bannercontainer">
             <div className="banner">
-            <img src={logo} alt="logo"/>
+            <img src={logo} alt="logo" className="bannerlogo"/>
                 <div className="bannerbar">
                     {/*<Link to='/products?f=new' className="bannerbutton">New Arrivals</Link>*/}
                     <Link to='/products?f=all' className="bannerbutton">Our Beers</Link>
@@ -13,6 +15,7 @@ const Header = ({ title }) => {
                     {/*<Link to='/products?f=nofun' className="bannerbutton">Non-Alcoholic</Link>*/}
                 </div>
             </div>
+            </Container>
             <div className="seperatorbar"></div>
         </header>
     )
